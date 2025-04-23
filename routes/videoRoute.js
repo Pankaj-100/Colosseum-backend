e3a10542ea655e8c93ca6a61574e8664cafdebe1
pankaj-100 <pankajtech1234@gmail.com>
2025-04-23 13:12:40 +0530

    minor

--- routes/videoRoute.js
+++ routes/videoRoute.js
@@ -9,10 +9,11 @@ const {  getUploadURL,
 const { auth, isAdmin } = require("../middlewares/auth");
 
 const router = express.Router();
+router.route("/uploads/generate-urls").post(auth, isAdmin, getUploadParts);
 router.route("/get-upload-url").post(auth, isAdmin, getUploadURL);
 router.route("/").get(auth, isAdmin, getVideos).post(auth, isAdmin, saveVideo);
 router.route("/uploads/initiate").post(auth, isAdmin, initiateMultipartUpload);
-router.route("/uploads/generate-urls").post(auth, isAdmin, getUploadParts);
+
 router.route("/uploads/complete").post(auth, isAdmin, completeMultipartUpload);
 router.route("/uploads/abort").post(auth, isAdmin, abortMultipartUpload);
 
