2aff95b48d074f4a89fd71793d47c860e12f55e5
pankaj-100 <pankajtech1234@gmail.com>
2025-04-28 18:22:06 +0530

    language

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     preferredLanguage: {
       type: String,
       required: true,
-      default: "English",
+      default: "Spanish",
     },
     verified: {
       type: Boolean,
